39841ac85992ba0a035bad0c59b73ed96cbb610c
Al3dekev <quentin1998@me.com>
2019-02-02 14:15:46 +0100

    Main app Beginning

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,37 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {MatButtonModule, MatCardModule, MatInputModule, MatListModule, MatToolbarModule} from "@angular/material";
+import { LeftColumnComponent } from './left-column/left-column.component';
+import { GameConfigurationComponent } from './game-configuration/game-configuration.component';
+import { GameStartedComponent } from './game-started/game-started.component';
+import { GamePlateauComponent } from './game-plateau/game-plateau.component';
+import { PlayerShipComponent } from './player-ship/player-ship.component';
+import { EnemyShipComponent } from './enemy-ship/enemy-ship.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeftColumnComponent,
+    GameConfigurationComponent,
+    GameStartedComponent,
+    GamePlateauComponent,
+    PlayerShipComponent,
+    EnemyShipComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatListModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/electronLaunch.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
+let win;
+function isDev() {
+  return process.mainModule.filename.indexOf('app.asar') === -1;
+};
+function createWindow() {
+  win = new BrowserWindow({
+    width: 1000,
+    height: 560,
+    fullscreen:false,
+    resizable:false
+  });
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, '../dist/Shipway/index.html'),
+    protocol: 'file',
+    slashes: true
+  }));
+  if (isDev()) {
+    win.webContents.openDevTools()
+  }
+  win.on('closed', () => {
+    win = null
+  })
+}
+app.on('ready', createWindow);
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+app.on('activate', () => {
+  if (win === null) {
+    createWindow()
+  }
+})
