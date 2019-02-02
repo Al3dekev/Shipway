075ba591079e35eb9f061f08c96ed6de3b228150
Al3dekev <quentin1998@me.com>
2019-02-02 21:32:30 +0100

    News: 1st establishment of the Left Column

--- src/app/left-column/left-column.component.ts
+++ src/app/left-column/left-column.component.ts
@@ -8,8 +8,37 @@ import { Component, OnInit } from '@angular/core';
 export class LeftColumnComponent implements OnInit {
 
   GameTitle:String;
+  MenuButtons:String[];
+  GameConfStatSel:Number=1;
 
   constructor() { }
+
+
+  playGame(){
+
+  };
+
+
+  getGameTitle(){
+    return this.GameTitle;
+  }
+  setGameTitle(set){
+    this.GameTitle = set;
+  }
+
+  getMenuButtons(){
+    return this.MenuButtons;
+  }
+  setMenuButtons(set){
+    this.MenuButtons = set;
+  }
+
+  getGameConfStatSel(){
+    return this.GameConfStatSel;
+  }
+  setGameConfStatSel(set){
+    this.GameConfStatSel=set;
+  }
 
 
 
