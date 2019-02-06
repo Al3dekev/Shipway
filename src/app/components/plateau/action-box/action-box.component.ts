6172f9ba5fea3e6298a4e6c5ef66b9354cb76bc7
Al3dekev <quentin1998@me.com>
2019-02-06 22:52:57 +0100

    News: Starting to create first idea for the game plateau

---
+++ src/app/components/plateau/action-box/action-box.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-action-box',
+  templateUrl: './action-box.component.html',
+  styleUrls: ['./action-box.component.less']
+})
+export class ActionBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/components/plateau/game-started/game-started.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-game-started',
+  templateUrl: './game-started.component.html',
+  styleUrls: ['./game-started.component.less']
+})
+export class GameStartedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
