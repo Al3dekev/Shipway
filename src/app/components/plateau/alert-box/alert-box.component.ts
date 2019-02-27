7a97efb3b489523e9aaf07398bd0f21b9e0949af
Al3dekev <quentin1998@me.com>
2019-02-27 19:19:06 +0100

    Update for AlertBox

--- src/app/components/plateau/alert-box/alert-box.component.ts
+++ src/app/components/plateau/alert-box/alert-box.component.ts
@@ -9,91 +9,17 @@ import {PlayGameService} from "../../../services/play-game.service";
 })
 export class AlertBoxComponent implements OnInit {
 
-  private _alertTypeList:Array<string>[] = [
-    ["spawn","contenue spawn"],
-    ["start","content"],
-    ["movement","content"],
-    ["attack","content"],
-    ["gotHit","content"],
-    ["turn","content"],
-    ["defeat","content"],
-    ["victory","content"]
-  ];
-
-  private _alertMainObject:Array<{id:number,type:string,content:string}> = [];
-
-  private _alertViewMainObjectList:Array<{id:number,type:string,content:string}> = [];
 
 
 
-// voir ou ça nous mene
 
 
-  constructor(private ats:AlertSenderService, private pgs:PlayGameService) {
-    this.initiateAlertDataListConversion();
-  }
 
-  addAlertInView(alertName:string){
-    let ship;
-    if(this.ats.actualTurnOwner){
-      ship = this.pgs.playerShipName;
-    }else{
-      ship = this.pgs.enemyShipName;
-    }
-    console.log("this.alertMainObject.length: "+this.alertMainObject.length);
-    for(let x=0;x < this.alertMainObject.length;x++){
-      if(alertName == this.alertTypeList[x][0]){
-        this.alertMainObject.push({
-          id: x,
-          type:this.alertTypeList[x][0],
-          content:ship+this.alertTypeList[x][1]
-        })
-      }
-    }
+
+  constructor(public ats:AlertSenderService, private pgs:PlayGameService) {
   }
 
 
-
-/*  addAlertMessage(type:string){
-    this.alertTypeList.forEach(obj =>{
-      if(obj)
-    })
-  }*/
-
-  initiateAlertDataListConversion(){
-      console.log("this.alertMainObject.length: "+this.alertMainObject.length);
-    for(let x=0;x < this.alertMainObject.length;x++){
-      this.alertMainObject.push({
-        id: x,
-        type:this.alertTypeList[x][0],
-        content:this.alertTypeList[x][1]
-      })
-    }
-  }
-
-  get alertTypeList(): Array<string>[] {
-    return this._alertTypeList;
-  }
-
-  set alertTypeList(value: Array<string>[]) {
-    this._alertTypeList = value;
-  }
-
-  get alertMainObject(): Array<{ id: number; type: string; content: string }> {
-    return this._alertMainObject;
-  }
-
-  set alertMainObject(value: Array<{ id: number; type: string; content: string }>) {
-    this._alertMainObject = value;
-  }
-
-  get alertViewMainObjectList(): Array<{ id: number; type: string; content: string }> {
-    return this._alertViewMainObjectList;
-  }
-
-  set alertViewMainObjectList(value: Array<{ id: number; type: string; content: string }>) {
-    this._alertViewMainObjectList = value;
-  }
 
 
   ngOnInit() {
--- src/app/services/alert-sender.service.ts
+++ src/app/services/alert-sender.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import {PlayGameService} from "./play-game.service";
 
 @Injectable({
   providedIn: 'root'
@@ -6,28 +7,119 @@ import { Injectable } from '@angular/core';
 export class AlertSenderService {
 
 
-private _alertTypeEvent:string[];
 
   private _actualTurnOwner:boolean;
 
+  private _alertTypeList:any[];
+
+  private _alertMainObject:Array<{id:number,type:string,content:string}> = [];
+
+  private _alertViewMainObjectList:Array<{id:number,type:string,content:string}> = [];
 
 
-  constructor() {
+  constructor(private pgs:PlayGameService) {
+
+    this.initiateAlertDataListConversion;
+
+  }
+
+  addAlertInView(alertName:string){
+
+    console.log("this.alertMainObject.length: "+this.alertTypeList.length);
+
+    let x:number = 0;
+
+
+
+    for(let aMO of this.alertMainObject){
+
+      if(alertName == aMO.type){
+        this.alertViewMainObjectList.push({
+          id: x,
+          type:aMO.type,
+          content:aMO.content
+        })
+      }
+      x++;
+    }
+
+
+  }
+
+  /**
+   * initialise la liste des mesages
+   * @constructor
+   */
+  AlertMessagesArrays(){
+    let Attship,DefShip;
+    let pref = "The ";
+    if(this.actualTurnOwner){
+      Attship = pref+this.pgs.playerShipName;
+      DefShip = pref+this.pgs.enemyShipName;
+    }else{
+      DefShip = pref+this.pgs.playerShipName;
+      Attship = pref+this.pgs.enemyShipName;
+    }
+
+    this.alertTypeList = [
+      ["spawn",Attship+" has spawned on the map"],
+      ["start","The game is initialized"],
+      ["end","The game is ended"],
+      ["movement",Attship+" moved"],
+      ["attack",Attship+" attacked"],
+      ["gotHit",DefShip+" been hit"],
+      ["missHit", "...and it missed"],
+      ["turn","TURN OF "+Attship],
+      ["defeat",Attship+" lost."],
+      ["victory",Attship+" won."]
+    ];
+
+
   }
 
 
-  addAlertTypeEvent(typeToAdd:string){
-    this.alertTypeEvent.push(typeToAdd);
 
+
+  /**
+   * Initialise la liste des messages dans un tableau d'objets
+   */
+  initiateAlertDataListConversion(){
+
+    this.AlertMessagesArrays;
+
+    console.log("this.alertMainObject.length: "+this.alertMainObject.length);
+    for(let x=0;x < this.alertTypeList.length;x++){
+      this.alertMainObject.push({
+        id: x,
+        type:this.alertTypeList[x][0],
+        content:this.alertTypeList[x][1]
+      })
+    }
+  }
+
+  get alertTypeList(): any[] {
+    return this._alertTypeList;
+  }
+
+  set alertTypeList(value: any[]) {
+    this._alertTypeList = value;
   }
 
 
-  get alertTypeEvent(): string[] {
-    return this._alertTypeEvent;
+  get alertMainObject(): Array<{ id: number; type: string; content: string }> {
+    return this._alertMainObject;
   }
 
-  set alertTypeEvent(value: string[]) {
-    this._alertTypeEvent = value;
+  set alertMainObject(value: Array<{ id: number; type: string; content: string }>) {
+    this._alertMainObject = value;
+  }
+
+  get alertViewMainObjectList(): Array<{ id: number; type: string; content: string }> {
+    return this._alertViewMainObjectList;
+  }
+
+  set alertViewMainObjectList(value: Array<{ id: number; type: string; content: string }>) {
+    this._alertViewMainObjectList = value;
   }
 
   get actualTurnOwner(): boolean {
