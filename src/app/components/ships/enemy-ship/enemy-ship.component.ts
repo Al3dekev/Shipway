888aa916568a515eef35821fc8dbfc8905c2a5ab
Al3dekev <quentin1998@me.com>
2019-03-13 18:30:52 +0100

    Minor JS EnemyShip update

--- src/app/components/ships/enemy-ship/enemy-ship.component.ts
+++ src/app/components/ships/enemy-ship/enemy-ship.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AlertSenderService} from "../../../services/alert-sender.service";
 
 @Component({
   selector: 'enemy-ship',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EnemyShipComponent implements OnInit {
 
-  constructor() { }
+  constructor(private alSender:AlertSenderService) {
+    this.alSender.addAlertInView("spawn");
+  }
 
   ngOnInit() {
   }
